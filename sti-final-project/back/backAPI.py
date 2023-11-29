27326a4be80bcc4fac2efe0b83acd1577a65f3d9
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2023-11-29 10:48:46 -0500

    Flask app crossover def implemented with firebase existing data

--- sti-final-project/back/backAPI.py
+++ sti-final-project/back/backAPI.py
@@ -8,110 +8,79 @@ from sklearn.metrics.pairwise import cosine_similarity
 app = Flask(__name__)
 CORS(app)
 
-# Datos de ejemplo (usuarios y elementos)
-usuarios = {
-    'Usuario1': {
-      "crowdedness": 2,
-      "noise": 4,
-      "lighting": 8,
-      "cold_temperature": 2,
-      "hot_temperature": 1,
-      "comfort": 3,
-      "services": 1,
-      "electrical_outlets": 9
-    },
-    'Usuario2': {
-        "crowdedness": 8,
-        "noise": 9,
-        "lighting": 7,
-        "cold_temperature": 5,
-        "hot_temperature": 1,
-        "comfort": 5,
-        "services": 5,
-        "electrical_outlets": 1
-      },
-    'Monk': {
-        "crowdedness": 3,
-        "noise": 1,
-        "lighting": 1,
-        "cold_temperature": 1,
-        "hot_temperature": 1,
-        "comfort": 7,
-        "services": 5,
-        "electrical_outlets": 1
-      }
-    # Agrega más usuarios si es necesario
-}
+# Initialize Firebase with project credentials
+cred = credentials.Certificate('icesi-spaces-firebase-adminsdk-z0bds-cc2b709bb4.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://icesi-spaces-default-rtdb.firebaseio.com/'
+})
 
-elementos = {
-    'space 1': {
-      "crowdedness": 3,
-      "noise": 2,
-      "lighting": 5,
-      "cold_temperature": 4,
-      "hot_temperature": 6,
-      "comfort": 4,
-      "services": 2,
-      "electrical_outlets": 5
-    },
-    'space 2': {
-      "crowdedness": 8,
-      "noise": 3,
-      "lighting": 8,
-      "cold_temperature": 8,
-      "hot_temperature": 2,
-      "comfort": 8,
-      "services": 8,
-      "electrical_outlets": 10
-    },
-    'space 3': {
-      "crowdedness": 8,
-      "noise": 7,
-      "lighting": 7,
-      "cold_temperature": 3,
-      "hot_temperature": 7,
-      "comfort": 7,
-      "services": 10,
-      "electrical_outlets": 7
-    },
-    # Agrega más elementos si es necesario
-}
+# Get Firebase database reference 
+db_ref = db.reference()
+
+
+# Get data from root: "database_people"
+db_people_ref = db.reference('database_people')
+
+# Get data from root:  "database_spaces"
+db_spaces_ref = db.reference('database_spaces')
+
+# Variables with people and spaces data
+data_people = db_people_ref.get()
+data_spaces = db_spaces_ref.get()
+
+# Set users and spaces for recomendation
+if data_people:
+    usuarios = {str(i): user_data for i, user_data in enumerate(data_people)} if data_people else {}
+else:
+    usuarios = {}
+
+if data_spaces:
+    if data_spaces:
+      elementos = {
+        str(i): {k: v for k, v in space.items() if k not in ['name', 'image', 'description']}
+        for i, space in enumerate(data_spaces)
+    }
+else:
+    elementos = {}
 
 # Función para obtener recomendaciones para un usuario específico
-def obtener_recomendaciones(usuario, usuarios, elementos):
+def obtener_recomendaciones(usuario, usuarios, espacios):
     recomendaciones = {}
-    for pelicula in elementos:
-        # Verificar si el usuario no ha visto la película
-        if pelicula not in usuario:
-            similitud = cosine_similarity([list(usuario.values())], [list(elementos[pelicula].values())])[0][0]
-            recomendaciones[pelicula] = similitud
+    for espacio in espacios:
+        # Verificar si el usuario no ha visitado el espacio
+        if espacio not in usuario:
+            similitud = cosine_similarity([list(usuario.values())], [list(espacios[espacio].values())])[0][0]
+            recomendaciones[espacio] = similitud
     return sorted(recomendaciones.items(), key=lambda x: x[1], reverse=True)
 
 # Usuario para el que deseamos hacer recomendaciones
-usuario_a_recomendar = 'Usuario1'
+usuario_a_recomendar = '0'  # Puedes cambiar esto al usuario que desees
 
 # Obtener recomendaciones para el usuario
 recomendaciones = obtener_recomendaciones(usuarios[usuario_a_recomendar], usuarios, elementos)
 
 # Mostrar las recomendaciones
-print(f'Recomendaciones para {usuario_a_recomendar}: {recomendaciones}')
+print(f'Recomendaciones para el Usuario {usuario_a_recomendar}: {recomendaciones}')
 
 
-# Ruta para obtener recomendaciones
+# Route for getting recomendations
 @app.route('/api/recommendations', methods=['POST'])
-def recommend_movies():
+def recommend_spaces():
     data = request.get_json()
+
+    if 'user_id' not in data:
+        return jsonify({'error': 'User ID is required'}), 400
     
-    if 'user' not in data:
-        return jsonify({'error': 'User data is required'}), 400
-    
-    user_data = data['user']
-    
-    if user_data not in usuarios:
+    user_id = data['user_id']
+
+    if user_id not in usuarios:
         return jsonify({'error': 'User not found'}), 404
 
-    recommendations = obtener_recomendaciones(usuarios[user_data], usuarios, elementos)
-    return jsonify({'user': user_data, 'recommendations': recommendations})
+    # Obtener recomendaciones para el usuario correspondiente al ID
+    recommendations = obtener_recomendaciones(usuarios[user_id], usuarios, elementos)
+    
+    return jsonify({'user_id': user_id, 'recommendations': recommendations}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
